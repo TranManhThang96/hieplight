6f561b1a78b1f068e19ff26b43d82ff69d6ffecc
TranManhThang <tranmanhthang96@gmail.com>
2023-11-30 22:51:28 +0700

    xu ly 3 khoi

--- wp-content/themes/volta/page-96.php
+++ wp-content/themes/volta/page-96.php
@@ -20,7 +20,7 @@
           </div>
           <div class="tp-caption fade" data-x="right" data-y="center" data-hoffset="0" data-voffset="65" data-speed="1000" data-start="800" data-easing="easeInQuint">
             <h6 class="sl_p poppins">
-            <?php echo get_field('noi_dung_quang_cao') ?>
+              <?php echo get_field('noi_dung_quang_cao') ?>
             </h6>
           </div>
           <div class="tp-caption sfb" data-x="right" data-y="center" data-hoffset="0" data-voffset="145" data-speed="1000" data-start="800" data-easing="easeOutBounce">
@@ -31,8 +31,60 @@
         </li>
 
       <?php }
+      wp_reset_query();
       ?>
     </ul>
+  </div>
+</section>
+
+<?php
+$title1 = get_field('tieu_de_1');
+$image1 = get_field('anh_dich_so_1');
+$link1 = get_field('link_dich_vu_so_1');
+
+$title2 = get_field('tieu_de_2');
+$image2 = get_field('anh_dich_vu_so_2');
+$link2 = get_field('link_dich_vu_so_2');
+
+
+$title3 = get_field('tieu_de_3');
+$image3 = get_field('anh_dich_vu_so_3');
+$link3 = get_field('link_dich_vu_so_3');
+?>
+<section class="comonSection">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="singleCats">
+          <img src="<?php echo $image1 ?>" alt="<?php echo $title1 ?>" />
+          <div class="catContent poppins">
+            <a href="<?php echo '/hieplight/'.$link1 ?>"><?php echo $title1 ?></a>
+          </div>
+          <div class="hoverEffect"></div>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="singleCats">
+          <img src="<?php echo $image2 ?>" alt="<?php echo $title2 ?>" />
+          <div class="catContent poppins">
+            <a href="<?php echo '/hieplight/'.$link2 ?>"><?php echo $title2 ?></a>
+          </div>
+          <div class="hoverEffect"></div>
+        </div>
+      </div>
+
+
+      <div class="col-sm-4">
+        <div class="singleCats">
+          <img src="<?php echo $image3 ?>" alt="<?php echo $title3 ?>" />
+          <div class="catContent poppins">
+            <a href="<?php echo '/hieplight/'.$link3 ?>"><?php echo $title3 ?></a>
+          </div>
+          <div class="hoverEffect"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 
